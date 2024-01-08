cabbfd12fe827f93b10df87698dc7404fb760835
Mohan Tayade <mohantayade326@gmail.com>
2024-01-08 10:50:11 +0530

    Util Code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // require('dotenv').config({path:'./.env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import app from "./app.js"
 
 dotenv.config({
     path: './.env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(` Server Is Running at Port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("DB conection error or faild" , error);
+})
