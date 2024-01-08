9331f8ee618d497d06ff1d1b89c3e0a0514e0d1f
Mohan Tayade <mohantayade326@gmail.com>
2024-01-08 15:46:15 +0530

    all config setup clear

--- src/app.js
+++ src/app.js
@@ -13,4 +13,17 @@ app.use(express.urlencoded({extended: true,limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+// routes imports ************
+
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes decleration 
+app.use("/api/v1/users", userRouter)
+
+
+
+
 export default app;
