2d0f467b607adfe327bd2f77908ef2fd630fef34
Mohan Tayade <mohantayade326@gmail.com>
2024-01-09 16:19:38 +0530

    subscribe model is created

---
+++ src/models/subscription.model.js
@@ -0,0 +1,18 @@
+import mongoose , {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId, //one who is subscribe
+        ref:"User"
+    },
+    channel:{
+        type: Schema.Types.ObjectId, //one whom to "subscriber" is subscribing
+        ref:"User"
+    }
+},
+{
+    timestamps: true
+})
+
+
+export const Subscription = mongoose.model("subscription",subscriptionSchema)
