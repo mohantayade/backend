5b186044fc40ed0da9464b929b33c2d76cbfe50d
Mohan Tayade <mohantayade326@gmail.com>
2024-01-11 14:58:32 +0530

    pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponce } from "../utils/ApiResponce.js"
 import jwt  from "jsonwebtoken"
+import mongoose from "mongoose"
 
 const generateAccessTokenAndRefreshToken = async (userID)=>{
   try {
@@ -338,4 +339,50 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
      return res.status(200).json(new ApiResponce(200, channel[0],"user channel fetched Successfully"))
 })
 
-export {registerUser,loginUser,logOutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile};
+const getWatchHistory = asyncHandler(async(req,res)=>{
+      const user = await User.aggregate([
+        {
+          $match:{
+            _id: new mongoose.Types.ObjectId(req.user._id)
+          }
+        },
+        {
+          $lookup:{
+            from: "videos",
+            localField: "watchHistory",
+            foreignField: "_id",
+            as: "watchHistory",
+            pipeline: [
+              {
+                $lookup:{
+                  from: "users",
+                  localField: "owner",
+                  foreignField: "_id",
+                  as: "owner",
+                  pipeline:[
+                    {
+                      $project:{
+                        fullname: 1,
+                        username: 1,
+                        avatar: 1
+                      }
+                    }
+                  ]
+                }
+              },
+              {
+                $addFields:{
+                  owner:{
+                    $first: "$owner"
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ])
+
+      return res.status(200).json(new ApiResponce(200,user[0].watchHistory,"watch history fetched Successfully"))
+})
+
+export {registerUser,loginUser,logOutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory};
